300ccc79c91b82638df1b4d7cf302a4561685206
miguelsumio <mv2844407@gmail.com>
2026-05-21 15:26:55 -0300

    Add files via upload

--- curriculo/js/script.js
+++ curriculo/js/script.js
@@ -1,38 +1,43 @@
-const botao = document.getElementById("tema");
+/*-const botao = document.getElementById("Tema");
 
-        //carregar preferencia
-        if(localStorage.getItem("tema") === "dark") {
-        document.body.classList.add("dark")
-         }
+if (localStorage.getItem("Tema") === "dark"){
+    document.body.classList.add("dark");
+}
 
-         //alternar tema 
-         botao.addEventListener("click", () => {
-            document.body.classList.toggle("dark");
+botao.addEventListener("click", () => { 
+    document.body.classList.toggle("dark");
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("Tema", "dark");
+    } else {
+        localStorage.setItem("Tema", "light");
+    }
+});*/
 
-        if (document.body.classlist.contains("dark")) {
-        localStorage.setItem("tema", "dark");
-        
+const botao= document.getElementById("tema");
+
+const updateButtonIcon = () => {
+    if (!botao) return;
+    const darkMode = document.body.classList.contains("dark")
+    botao.textContent = darkMode ? "​☀️​" : "🌑";
+    botao.title = darkMode ? "Mudar para modo claro" : "Mudar para modo escuro";
+};
+
+const TemaAtual = localStorage.getItem("tema") === "dark" ? "dark" : "light";
+if(TemaAtual === "dark") {
+    document.body.classList.add("dark");
+}
+
+updateButtonIcon();
+
+if (botao) {
+    botao.addEventListener("click", () => { document.body.classList.toggle("dark");
+
+        if(document.body.classList.contains("dark")) {
+            localStorage.setItem("tema", "dark");
         } else {
             localStorage.setItem("tema", "light");
         }
-        });
-        {
-        const botao = document.getElementById("tema");
-        
-        const updatebuttonicon = () => {
-         if (botao) return;
-         const darkmode = document.body.classlist.contains("dark");
-         botao.textcontent = darkmode ? "​☀️​" : "🌑";
-         botao.title = darkmode ? "mudar para o modo claro" : "mudar para o modo escuro";
-  };
-     const temaatual = localStorage.getitem("tema") === "dark" ? "dark" :
-     "light";
-     if (temaatual === "dark") {
-        document.body.classlist.add("dark");
-        }
-     updatebuttonicon();
-    
-     if (botao) {
-      
-     }
-    }
+
+        updateButtonIcon ();
+    });
+}
